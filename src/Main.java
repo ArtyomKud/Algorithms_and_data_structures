b822500315d9b197ead497ae44017417ef2ef417
Artyom <artyom_kudryavcev_91@mail.ru>
2023-07-30 20:21:12 +0700

    решил домашнюю №3

--- src/Main.java
+++ src/Main.java
@@ -3,51 +3,35 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
+       /*HomeWork2
         int [] array = {1,4, 2, 8, 56, 34, 28, 67};
         System.out.println(Arrays.toString(array));
-        heapSort(array);
-        System.out.println(Arrays.toString(array));
+        HomeWork2.heapSort(array);
+        System.out.println(Arrays.toString(array)); */
 
-
-
-    }
-
-    public static void heapSort(int[] arr) {
-        for(int i = arr.length/2-1; i>=0; i--){
-            heapBuilding(arr, arr.length, i);
-        }
-
-        for(int i = arr.length-1; i >= 0; i--){
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
-            heapBuilding(arr, i, 0);
-        }
-
-    }
-
-
-
-    public static void heapBuilding(int[] arr, int lendhth, int number) {
-        int parent = number;
-        int leftChild = 2 * number +1;
-        int rightChild = 2 * number + 2;
-
-        if(leftChild < lendhth && arr[parent] < arr[leftChild]){
-            parent = leftChild;
-        }
-        if(rightChild < lendhth && arr[parent] < arr[rightChild]){
-            parent = rightChild;
-        }
-
-        if(parent != number){
-            int temp = arr[number];
-            arr[number] = arr[parent];
-            arr[parent] = temp;
-
-            heapBuilding(arr, lendhth, parent);
-        }
+        //HomeWork3
+        HomeWork3 homeWork3 = new HomeWork3();
+        homeWork3.addLast(1);
+        homeWork3.addLast(2);
+        homeWork3.addLast(3);
+        homeWork3.addLast(4);
+        homeWork3.addLast(5);
+        homeWork3.addLast(6);
+        homeWork3.addLast(7);
+        homeWork3.addLast(8);
+        System.out.println(homeWork3.printNode());
+        homeWork3.reverse();
+        System.out.println(homeWork3.printNode());
 
 
     }
+
+
+
+
+
+
+
+
 }
